8a2d37f6ae6746a7862fa6c0750454a5c78662bf
hustoncun <1kor.oleg@gmail.com>
2020-10-17 17:50:24 +0300

    make urls

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,37 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
+from django.contrib.auth import views as auth_views
 
-import apps.forms.views
+# Custom login
+import core.static
+from core.admin import site
 
+admin.site.login = site.login
+
+# admin page
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('forms/', apps.forms.views.index)
+]
+
+# auth/login
+urlpatterns += [
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+]
+
+# passes_manager
+urlpatterns += [
+    path('', include('apps.passes_manager.urls'))
+]
+
+# serving staticfiles/media
+urlpatterns += [
+    url(r'^media/(?P<path>.*)$', core.static.serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]
