c641c7db2c429a14aa214c3642a9facd7f8543d6
hustoncun <1kor.oleg@gmail.com>
2020-10-19 14:51:44 +0300

    configure runserver

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,15 +1,13 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent
 
 command = BASE_DIR / 'venv/bin/gunicorn'
 pythonpath = BASE_DIR
-bind = '127.0.0.1:8001'
+bind = os.getenv('EXTERNAL_HOST')
 workers = 2
 user = 'www'
 limit_request_fields = 32000
 limit_request_field_size = 0
-raw_env = 'DJANGO_SETTINGS_MODULE=config.settings.production'
-
-if __name__ == '__main__':
-    print(BASE_DIR)
+raw_env = 'DJANGO_SETTINGS_MODULE=config.settings'
